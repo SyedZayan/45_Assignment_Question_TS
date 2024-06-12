43ddf4a2a88144e42fe84fc992099979c2e8f84d
@Syed Zayan Ali <96523088+SyedZayan@users.noreply.github.com>
2024-06-13 02:49:27 +0500

    Add files via upload

---
+++ 2_personal_message/2_personal_message.js
@@ -0,0 +1,4 @@
+"use strict";
+// Task No-02:
+let peronName = "Eric";
+console.log(`Hello ${peronName} ,would you like to learn some python today?`);
---
+++ 2_personal_message/2_personal_message.ts
@@ -0,0 +1,5 @@
+// Task No-02:
+
+let peronName: string = "Eric";
+console.log(`Hello ${peronName} ,would you like to learn some python today?`);
+
